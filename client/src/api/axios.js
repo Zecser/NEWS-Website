f877f5de4a5a57c6e4e61f1fa6c3a77cf914fcdc
aravind <aravind284479@gmail.com>
2025-08-01 17:52:21 +0530

    feat : url fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const baseURL = import.meta.env.VITE_API_URL;
+export const baseURL = import.meta.env.VITE_API_URL || 'https://news-website-4xw6.onrender.com';
+
 
 const API = axios.create({
   baseURL,
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { baseURL } from "./src/api/axios";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: baseURL,
+        target: "https://news-website-4xw6.onrender.com",
       },
     },
   }
