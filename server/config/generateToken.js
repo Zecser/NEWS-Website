47c13bfb92322a52cb41b7bc0917e402f0a6fcd1
aravind <aravind284479@gmail.com>
2025-08-02 19:15:06 +0530

    feat : changed code

--- server/config/generateToken.js
+++ server/config/generateToken.js
@@ -8,10 +8,10 @@ export const generateToken = (res, user, message, role) => {
   return res
     .status(200)
     .cookie("token", token, {
-      expires: new Date(Date.now() + 24 * 60 * 60 * 1000), //1day
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: true,
       sameSite: "none",
-      secure: process.env.NODE_ENV === "production",
     })
     .json({ success: true, message, user });
 };
